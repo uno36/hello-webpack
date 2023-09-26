b166bfaf1b930afca24d9d20d77a79e9daf66cea
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-26 22:05:32 +0200

    Create Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello, how are you today?",
+  "Good morning! Have a great day!",
+  "Hi there! What's on your mind?",
+  "Hey, hope you're doing well!",
+  "Greetings! What's new?"
+]
+
+greetings.each do |greeting|
+  Message.create(content: greeting)
+end
